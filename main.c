f617a25d525a7ce7de21cac654f9079b2f169c2e
20161104581 <547329654@qq.com>
2017-06-19 11:06:41 +0800

    shuixianhua
    /Users/c20161104581/Desktop/cyuyantiku/cyuyantiku/main.c

---
+++ main.c
@@ -0,0 +1,33 @@
+//
+//  main.c
+//  cyuyantiku
+//
+//  Created by 20161104581 on 17/6/19.
+//  Copyright © 2017年 20161104581. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    int i,a,b,c,m,n,flag;
+    while(scanf("%d%d",&m,&n)!=EOF)
+    {
+        flag=1;
+        for(i=m;i<=n;i++)
+        {
+            a=i%10;
+            b=i/10%10;
+            c=i/100;
+            if(a*a*a+b*b*b+c*c*c==i)
+            {
+                printf("%d",i);
+                flag=0;
+            }
+        }
+        if(flag)
+            printf("no\n");
+        else
+            printf("\n");
+    }
+        return 0;
+}
